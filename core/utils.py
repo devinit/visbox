15d16b8c60316776b3cd9f0c4b35c1e8de1bca00
Alex Miller <alex.k.miller@gmail.com>
2017-05-08 17:52:36 +0000

    Export to PNG button

--- core/utils.py
+++ core/utils.py
@@ -4,11 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def chromePNG(url,tmpf):
+    padding_right = 0
+    padding_bottom = 0
     options = webdriver.ChromeOptions()
     
     options.add_argument('headless')
-    options.add_argument('disable-gpu')
     options.add_argument('window-size=2000x2000')
+    options.add_argument("start-maximized")
     
     driver = webdriver.Chrome(executable_path='/src/chromedriver_linux64/chromedriver',chrome_options=options)
     driver.get(url)
@@ -23,8 +25,8 @@ def chromePNG(url,tmpf):
     
     left = int(location['x'])
     top = int(location['y'])
-    right = int(location['x']) + min(int(size['width']),width)
-    bottom = int(location['y']) + min(int(size['height']),height)
+    right = int(location['x']) + min(int(size['width']),width) + padding_right
+    bottom = int(location['y']) + min(int(size['height']),height) + padding_bottom
     
     
     im = im.crop((left, top, right, bottom)) # defines crop points
