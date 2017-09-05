1a4c5885c434e3d1945caec283c32353f100859c
Alex Miller <alex.k.miller@gmail.com>
2017-09-05 12:11:23 -0400

    Pull DDW admin action

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,9 @@ from core.models import Dataset, Visualisation
 from django import forms
 from core.forms import *
 from django.forms import ModelForm
+from glob import glob
+from django.conf import settings
+from os.path import basename
 
 class UploadAdminForm(ModelForm):
     class Meta:
@@ -21,6 +24,16 @@ class UploadAdminForm(ModelForm):
         self.fields['sep'].strip = False
 
 # Register your models here.
+def pull_ddw(modeladmin,request,queryset):
+    ddw_datasets = glob(settings.STATIC_ROOT+'/core/data/*.csv')
+    for ddw_dataset in ddw_datasets:
+        dataset = Dataset()
+        dataset.name = basename(ddw_dataset)
+        dataset.data = open(ddw_dataset)
+        dataset.creator = User.objects.get(username=request.user)
+        dataset.sep=","
+        dataset.save()
+
 class DatasetAdmin(admin.ModelAdmin):
     #fields display on change list
     list_display = ['name','creator','created','sep']
@@ -28,6 +41,7 @@ class DatasetAdmin(admin.ModelAdmin):
     save_on_top = True
     #Pull in custom form
     form = UploadAdminForm
+    actions = [pull_ddw]
     
 class VisualisationAdmin(admin.ModelAdmin):
     #fields display on change list
